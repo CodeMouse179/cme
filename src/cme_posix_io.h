681144cf4e5a602b0dbfcf658f45cb2c030b4daf
CodeMouse179 <codemouse179@gmail.com>
2023-04-09 21:07:55 +0300

    update io

--- src/cme_posix_io.h
+++ src/cme_posix_io.h
@@ -1,3 +1,12 @@
 #ifndef CME_POSIX_IO_H
 #define CME_POSIX_IO_H
+
+#include "cme_system.h"
+
+#if defined(SYSTEM_POSIX)
+#include <unistd.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#endif
+
 #endif
--- src/cme_windows_io.h
+++ src/cme_windows_io.h
@@ -1,3 +1,10 @@
 #ifndef CME_WINDOWS_IO_H
 #define CME_WINDOWS_IO_H
+
+#include "cme_system.h"
+
+#if defined(SYSTEM_WINDOWS)
+#include <Windows.h>
+#endif
+
 #endif
